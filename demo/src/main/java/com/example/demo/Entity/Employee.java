f72bca87719dfb63071ba2c26d968189e50f2141
ParamiJayasinghe <apurvajayasinghe@gmail.com>
2024-10-17 22:07:18 +0530

    few changes

--- demo/src/main/java/com/example/demo/Entity/Employee.java
+++ demo/src/main/java/com/example/demo/Entity/Employee.java
@@ -1,4 +1,22 @@
 package com.example.demo.Entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@Data
+
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private  String Phone;
+    private String department;
+
 }
