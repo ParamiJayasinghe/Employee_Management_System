0def5c372f57786c8c0d1c509ab4b054e013c6a4
ParamiJayasinghe <apurvajayasinghe@gmail.com>
2024-10-19 20:41:21 +0530

    few changes in service

--- demo/src/main/java/com/example/demo/Service/EmployeeService.java
+++ demo/src/main/java/com/example/demo/Service/EmployeeService.java
@@ -1,7 +1,15 @@
 package com.example.demo.Service;
 
+import com.example.demo.Entity.Employee;
+import com.example.demo.Repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequireAgsConstructor
 public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public Employee postEmployee(Employee,employee)
+        return employeeRepository.save(employee)
 }
